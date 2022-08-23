1d101225133d222f855d1e3de6b7ffd1f90fbcc9
zevtyardt <zevtyardt>
2022-08-23 12:25:43 +0700

    refactor code

--- webcrawler/settings.py
+++ webcrawler/settings.py
@@ -2,21 +2,15 @@ from scrapy.utils.log import configure_logging
 from rich.logging import RichHandler
 import logging
 
-BOT_NAME = 'Spider'
-
-SPIDER_MODULES = ['Spider.spiders']
-NEWSPIDER_MODULE = 'Spider.spiders'
-
 LOG_ENABLED = False
 configure_logging(install_root_handler=False)
 
 FORMAT = "%(message)s"
 logging.basicConfig(
-    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(show_time=False, show_path=False, level=logging.DEBUG)]
+    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(show_time=False, show_path=False, level=logging.INFO)]
 )
 
 BOT_NAME = 'webcrawler'
-
 SPIDER_MODULES = ['webcrawler.spiders']
 NEWSPIDER_MODULE = 'webcrawler.spiders'
 
@@ -28,3 +22,7 @@ EXTENSIONS = {
 ITEM_PIPELINES = {
     'webcrawler.pipelines.ProcessPipeline': 300,
 }
+
+STATS_DUMP = False
+CONCURRENT_ITEMS = 1
+CONCURRENT_REQUESTS = 5
