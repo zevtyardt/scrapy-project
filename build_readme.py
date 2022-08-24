6e4af2619538951a210f2a263e838b218d8cf854
zevtyardt <zevtyardt>
2022-08-24 12:38:25 +0700

    update generator

--- build_readme.py
+++ build_readme.py
@@ -2,31 +2,44 @@ import sqlite3
 from pathlib import Path
 import textwrap
 
-md = ""
+md = """
+# Scrapy based Projects
+These are collection of some of my web scraping projects with scrapy. Feel free to use them, modify them and suggest modifications.
+
+## Requirements:
+1. [Python3.x](https://www.python.org/)
+2. [Scrapy framework](https://scrapy.org/)
+3. [Sqlalchemy](https://sqlalchemy.org)
+
+see: [requirements.txt](/requirements.txt)
+
+"""
 
 db = Path("database/database.sqlite")
+
 if db.is_file():
+    md += "## List Crawlers:\n"
     conn = sqlite3.connect('%s' % db)
     cur = conn.cursor()
 
-    table = ""
-
-    convert = lambda x, _=None: "|" + "|".join(f" {i} " if not _ else i for i in x) + "|\n"
+    convert = lambda x, _=None: "   |" + "|".join(f" {i} " if not _ else i for i in x) + "|\n"
     for db_name in cur.execute("select name from sqlite_schema where type='table'").fetchall():
-        c = cur.execute("select * from %s where id in (1, 2, 3, 4, 5)" % db_name)
+        table = ""
+        c = cur.execute("select * from %r where id in (1, 2, 3, 4, 5)" % db_name[0])
         data = c.fetchall()
-        if len(data) > 1:
+        if len(data) > 0:
             columns = list(next(filter(None, i)) for i in c.description)
 
             table += convert(columns)
             table += convert(("---" for i in columns), _=True)
             for item in data:
                 table += convert(map(lambda x: textwrap.shorten(str(x), 50), item))
-            table += "\n_and more.._\n\n"
+            table += "\n   _and more.._\n\n"
 
-        md += f"### {db_name[0].title()}\n"
+        md += f"1. **{':'.join(db_name[0].split('_'))}**\n"
         md += table
 
 if md:
     with open("README.md", "w") as fp:
         fp.write(md)
+    print ("done..")
