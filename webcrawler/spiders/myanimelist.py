ae3a3a0ad1c4602c85053926987725abf14c98e9
zevtyardt <zevtyardt>
2022-08-25 10:27:44 +0700

    add new crawler mal:character

--- webcrawler/spiders/myanimelist.py
+++ webcrawler/spiders/myanimelist.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import re
 
 class MyanimelistAnimeSpider(scrapy.Spider):
     name = 'mal:anime'
@@ -11,6 +11,7 @@ class MyanimelistAnimeSpider(scrapy.Spider):
             href = tr.css("a::attr(href)").get()
             if href:
                 yield scrapy.Request(url=href, callback=self.parse_item)
+        return
 
         next_page = response.css("a.next::attr(href)").get()
         if next_page:
@@ -19,16 +20,18 @@ class MyanimelistAnimeSpider(scrapy.Spider):
             yield scrapy.Request(url=next_page)
 
     def parse_item(self, response):
-        item = {"title": None}
+        item = {"title": None, "url": response.url}
         for data in response.css("table .spaceit_pad"):
             if data.css("span"):
-                _, key, *value = data.css("::text").extract()
+                # cleaning up
+                data = list(map(lambda y: y.strip(),
+                    filter(lambda x: x.strip("\n ,"),
+                      data.css("::text").extract()
+                )))
+
+                key, *value = data
                 if key.endswith(":"):
                     key = key[:-1].strip().lower()
-
-                # cleaning up
-                value = list(map(lambda y: y.strip(),
-                    filter(lambda x: x.strip("\n ,"), value)))
 
                 if len(value) == 1 or key in ("score", "ranked"):
                     value = value[0]
@@ -41,26 +44,47 @@ class MyanimelistAnimeSpider(scrapy.Spider):
         elif item.get("japanese"):
            item["title"] = item.pop("japanese")
 
-        item["synopsis"] = response.css("*[itemprop='description']::text").get()
+        item["synopsis"] = "".join(response.css("*[itemprop='description']::text").getall())
         yield item
 
 class MyanimelistMangaSpider(MyanimelistAnimeSpider):
     name = "mal:manga"
     start_urls = ['https://myanimelist.net/topmanga.php']
 
-    def parse_item(self, response):
-        pass
-
 class MyanimelistCharacterSpider(MyanimelistAnimeSpider):
     name = "mal:character"
     start_urls = ['https://myanimelist.net/character.php']
+    unique_key = "name"
 
     def parse_item(self, response):
-        pass
+        item = {"name": (response.css(".breadcrumb ~ .normal_header::text").get() or "").strip()}
+        item["url"] = response.url
+        item["japanese"] = (response.css(".breadcrumb ~ .normal_header small::text").get() or "").strip("()")
 
-class MyanimelistPeopleSpider(MyanimelistAnimeSpider):
+        source = re.search(f"[^(]+\(([^)]+)", response.css("title::text").get() or "")
+        if source:
+            item["source"] = source.group(1)
+
+        desc, bio = [], {}
+        for i in response.css("table td::text"):
+            text = i.get().strip()
+            if not text: continue
+            chara_info = re.search("^([a-zA-Z ]+): ([^>]+)", text)
+            if chara_info:
+                k, v = chara_info.groups()
+                bio[k.lower()] = v.strip()
+            else:
+                desc.append(text)
+        item["favorites"] = bio.pop("member favorites")
+        item["biodata"] = bio
+        item["description"] = "\n\n".join(desc)
+        yield item
+
+"""
+class MyanimelistPeopleSpider(MyanimelistCharacterSpider):
     name = "mal:people"
     start_urls = ['https://myanimelist.net/people.php']
 
     def parse_item(self, response):
         pass
+"""
